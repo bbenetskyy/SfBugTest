e65986317b4dd07935f14f31d9e5fc75ed9567b8
Bohdan Benetskyi <benetskyybogdan@gmail.com>
2023-06-07 08:10:05 +0200

    SfTextInputLayout iOS bug

---
+++ SfBugTest/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SfBugTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Screen_Display_Width = 100;
+            BindingContext = this;
+        }
+        
+        public double Screen_Display_Width { get; set; }
+    }
+}
